80a8849058f4f0bb9b95fa6968a376623da0a139
mariyamango <de.msomkova@gmail.com>
2024-09-03 10:53:58 +0200

    Added test

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void main_expectHelloWorld() {
+        Assertions.assertTrue(true);
+    }
+}
